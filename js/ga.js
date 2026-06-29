3799645e9fc78bb60319772a1a8e1ccc32eb07fb
MWmitsu <d.mitsu@myworks-corp.co.jp>
2026-06-30 06:49:06 +0900

    Create ga.js

---
+++ js/ga.js
@@ -0,0 +1,5 @@
+// Google Analytics 4 (gtag.js) 初期化 — CSP対応のため index.html のインラインを外部化
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-QBPFLVM9XP');
